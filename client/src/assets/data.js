5ec59ab78f0d4476640d9a958ea1db32a8df66fc
bharath <bharathac7@gmail.com>
2025-06-19 10:26:06 +0530

    v1

---
+++ client/src/assets/data.js
@@ -0,0 +1,14 @@
+export const summary = {
+    totalTasks: 10,
+    last10Task: [],
+    users: [],
+    tasks: { },
+}
+
+export const chartData = []
+
+export const tasks = []
+
+export const user = {}
+
+export const activitiesData = []

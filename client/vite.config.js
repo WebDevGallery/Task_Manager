b23ef367a14f16bc0b1c18e3d17c9dad014d3402
bharath <bharathac7@gmail.com>
2025-06-20 20:01:02 +0530

    v1

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 
     server: {
         port: 3000,
-        proxy: {
+        // proxy: {
         //     "/api": {
         //         target: "http://localhost:5000",
         //         changeOrigin: true,
